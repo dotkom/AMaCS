1585f200f1cbc1cdd70e92a1613c0ba0e504e66a
Jarle Trollebø <aborysa@gmail.com>
2017-07-30 12:12:36 +0200

    Create service for authenticating users

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -3,11 +3,27 @@ import React from "react";
 import { render } from "react-dom";
 
 
+//Setting up services
+import { ServiceManager, HttpServiceProvider, AuthServiceProvider } from 'services';
+
+import { OAUTH_SETTINGS } from 'common/constants';
+
+
+
+
+const serviceManager = new ServiceManager();
+serviceManager.registerService("http.service",HttpServiceProvider);
+serviceManager.alias("http","http.service");
+serviceManager.registerService("auth.service",AuthServiceProvider,OAUTH_SETTINGS);
+serviceManager.alias("auth","auth.service");
+
+
 function App(){
   return (
     <h1>Initial setup</h1>
-  )
+  );
 }
+
 
 class Root extends React.Component{
   render() {
---
+++ app/src/services/auth/auth.service.js
@@ -0,0 +1,79 @@
+import { UserManager, Log } from 'oidc-client';
+import { Observable, Subject, ReplaySubject } from 'rxjs';
+
+Log.logger = console;
+Log.level = Log.ERROR;
+
+export class AuthServiceProvider{
+  constructor(serviceManager,settings){
+    this.userManager = new UserManager(settings);
+    this._user = null;  
+    this.userSubject = new Subject();
+    this.userReplay = new ReplaySubject(1);
+    this._loginState = 0;
+    this.services = serviceManager;
+    this._initBindings();
+  }
+
+  _initBindings(){
+    this.userManager.events.addUserLoaded((user) => {
+      this.setToken(user,true);
+    })
+    
+    Observable.from(this.userManager.getUser()).subscribe((user) => {
+      this.setToken(user,true);
+    });
+
+    Observable.from(this.userManager.signinPopupCallback()).subscribe((user) => {
+      if(user)
+        location.href = "/";
+    });
+  }
+  
+  setToken(user,push){
+    console.log(user);
+    this._user = user;
+    this._loginState = 0;
+    //This side-effect might be a bit sneaky
+    this.services.getService("http").setAuth(user);
+    if(user)
+      Observable.from(this.userManager.getUser(user)).subscribe((...a) => {
+        console.log("getUser",...a);
+      });
+    if(push){
+      this.userSubject.next(user);
+      this.userReplay.next(user);
+    }
+  }
+
+  //Login
+  login(){
+    this._loginState = 1;
+    Observable.from(this.userManager.signinPopup()).flatMap((user)=> {
+      //this.setToken(user,true);
+    });
+    return this.getUser();
+  }
+  
+  logout(){
+    this._loginState = 2;
+    sessionStorage.clear();
+    this.setToken(null,true);
+    //Revoking the token does not work atm "405 Method not allowed"
+    /*Observable.from(this.userManager.signoutRedirect()).subscribe((user)=> {
+    });*/
+    return this.getUser();
+  
+  }
+
+  //Get user once
+  getUser(){
+    return this.userSubject.asObservable().take(1);
+  }
+
+  //Stream of users
+  onUserChange(){
+    return this.userReplay.asObservable();
+  }
+
+}
