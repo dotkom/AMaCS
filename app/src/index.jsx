9f296dd43afa8cc34c5993fc4ee51ecd7d77691f
Jarle Trollebø <aborysa@gmail.com>
2017-07-30 17:23:11 +0200

    Removed duplicate

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -20,14 +20,6 @@ serviceManager.registerService("auth.service",AuthServiceProvider,OAUTH_SETTINGS
 serviceManager.alias("auth","auth.service");
 
 
-
-
-const serviceManager = new ServiceManager();
-serviceManager.registerService("http.service",HttpServiceProvider);
-serviceManager.alias("http","http.service");
-serviceManager.registerService("auth.service",AuthServiceProvider,OAUTH_SETTINGS);
-serviceManager.alias("auth","auth.service");
-
 class Root extends React.Component{
   render() {
     return (
