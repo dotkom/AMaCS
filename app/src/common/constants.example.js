329cf07c65196eabc96eed38827cd1a094f5f254
Jarle Trollebø <aborysa@gmail.com>
2017-07-30 18:56:44 +0200

    Updated scope in constants example

--- app/src/common/constants.example.js
+++ app/src/common/constants.example.js
@@ -6,7 +6,7 @@ export const OAUTH_SETTINGS = {
   client_id: "...",
   response_type: "token",
   redirect_uri: "http://localhost:8080/auth",
-  scope: "authentication.onlineuser.username.read authentication.onlineuser.first_name.read authentication.onlineuser.last_name.read authentication.onlineuser.is_member.read authentication.onlineuser.field_of_study.read",
+  scope: "authentication.onlineuser.username.read authentication.onlineuser.first_name.read authentication.onlineuser.last_name.read authentication.onlineuser.email.read authentication.onlineuser.is_member.read authentication.onlineuser.field_of_study.read authentication.onlineuser.nickname.read authentication.onlineuser.rfid.read",
   metadata: {
     authorization_endpoint: "http://localhost:8000/sso/o/authorize/",
     userinfo_endpoint: "http://localhost:8000/sso/o/token/",
