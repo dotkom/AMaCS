5ae8593836fc5e956637c5b111ea621f75df97c0
Christian Duvholt <christian@duvholt.net>
2017-08-07 17:56:55 +0200

    clean up button design

--- app/src/components/Button.jsx
+++ app/src/components/Button.jsx
@@ -4,17 +4,13 @@ import _s from 'assets/css/Button.css';
 
 
 function Button({ children, onClick, text, iconLeft, iconRight }) {
-    return(
-        <button className={_s.button} onClick={onClick}>
-          { iconLeft ?
-            <img className={_s.iconLeft} src={iconLeft} /> : undefined
-          }
-          <p className={_s.text}>{ text }</p>
-          { iconRight ?
-            <img className={_s.iconRight} src={iconRight} /> : undefined
-          }
-        </button>
-    )
+  return(
+    <button className={_s.button} onClick={onClick}>
+      { iconLeft && <img className={_s.iconLeft} src={iconLeft} /> }
+      { text }
+      { iconRight && <img className={_s.iconRight} src={iconRight} /> }
+    </button>
+  )
 }
 
 export default Button;
