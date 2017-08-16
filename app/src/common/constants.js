acbcbce5e20f0b74d1065977cf7c1d7ecc9c8667
Håkon Solbjørg <hlsolbjorg@gmail.com>
2017-08-16 15:18:37 +0200

    Get SG configuration from environment

---
+++ app/src/common/constants.js
@@ -0,0 +1,12 @@
+export const OAUTH_SETTINGS = {
+  authority: process.env.SG_AUTH_AUTHORITY,
+  client_id: process.env.SG_AUTH_CLIENT_ID,
+  response_type: process.env.SG_AUTH_RESPONSE_TYPE,
+  redirect_uri: process.env.SG_AUTH_REDIRECT_URI,
+  scope: process.env.SG_AUTH_SCOPE,
+  metadata: {
+    authorization_endpoint: process.env.SG_AUTH_ENDPOINT_AUTH,
+    userinfo_endpoint: process.env.SG_AUTH_ENDPOINT_USER,
+    end_session_endpoint: process.env.SG_AUTH_ENDPOINT_REVOKE,
+  }
+}
