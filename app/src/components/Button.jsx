48eb4cd7a08214fefc5260a58d3447c15f6b834e
Håkon Solbjørg <hlsolbjorg@gmail.com>
2017-08-19 01:27:02 +0200

    Make button disablable

--- app/src/components/Button.jsx
+++ app/src/components/Button.jsx
@@ -3,9 +3,9 @@ import React from "react";
 import _s from 'assets/css/Button.css';
 
 
-function Button({ children, onClick, text, iconLeft, iconRight }) {
+function Button({ children, onClick, text, iconLeft, iconRight, disabled }) {
   return(
-    <button className={_s.button} onClick={onClick}>
+    <button className={_s.button} onClick={onClick} disabled={disabled}>
       { iconLeft && <img className={_s.iconLeft} src={iconLeft} /> }
       { text }
       { iconRight && <img className={_s.iconRight} src={iconRight} /> }
