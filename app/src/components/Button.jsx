3ed702cfb89567c209de4695effd5dc6933e6819
sklirg <hlsolbjorg@gmail.com>
2017-08-19 16:34:21 +0200

    Wrap button content in div to support flex

--- app/src/components/Button.jsx
+++ app/src/components/Button.jsx
@@ -6,9 +6,11 @@ import _s from 'assets/css/Button.css';
 function Button({ children, onClick, text, iconLeft, iconRight, disabled }) {
   return(
     <button className={_s.button} onClick={onClick} disabled={disabled}>
+      <div className={_s.buttonContent}>
       { iconLeft && <img className={_s.iconLeft} src={iconLeft} /> }
       { text }
       { iconRight && <img className={_s.iconRight} src={iconRight} /> }
+      </div>
     </button>
   )
 }
