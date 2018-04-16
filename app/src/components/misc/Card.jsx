794df31c8a720da2212b655ec001c52761936b80
mariush2 <MariusTobi_98@hotmail.com>
2018-04-16 21:22:45 +0200

    Added ingress to cards

--- app/src/components/misc/Card.jsx
+++ app/src/components/misc/Card.jsx
@@ -4,21 +4,85 @@ import classnames from 'classnames';
 import Button from 'components/misc/Button';
 
 import _s from 'assets/css/Card.scss';
-console.log(_s);
-export function Card({title, children, disabled}) {
-  return(
-    <div className={classnames(_s.card, {[_s.disabled]: disabled})}>
-      <div className={_s.top}>
-        <img src="../app/src/assets/images/online-icon-white.png"/>
-        <span>{title}</span>
-      </div>
-      {children}
-      <Button
-        text = "Til søknad"
-        className = {_s.cardButton}
-        disabled = { disabled }
-      >
-      </Button>
-    </div>
-  )
+
+class Card extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      title: props.title,
+      short: props.short,
+      children: props.children,
+      disabled: props.disabled,
+      ingress: false
+    }
+
+    this.showIngress = this.showIngress.bind(this);
+    this.hideIngress = this.hideIngress.bind(this);
+  }
+
+  showIngress() {
+    this.setState({ingress: true});
+  }
+
+  hideIngress() {
+    this.setState({ingress: false});
+  }
+
+  render() {
+    if(!this.state.disabled){
+      return(
+        <div className={_s.front}>
+          <div className={classnames(_s.card, {[_s.disabled]: this.state.disabled})}>
+            <div onClick={this.showIngress}>
+              <div className={_s.top}>
+                <img src="../app/src/assets/images/online-icon-white.png"/>
+                <span>{this.state.title}</span>
+              </div>
+              <p>{this.state.short}</p>
+            </div>
+            <Button
+              text = "Til søknad"
+              className = {_s.cardButton}
+              disabled = { this.state.disabled }
+            >
+            </Button>
+          </div>
+          <div onClick={this.hideIngress} className={classnames(_s.ingress, {[_s.enabled]: this.state.ingress})}>
+            <h1>{this.state.title}</h1>
+            <p>{this.state.children}</p>
+            <Button
+              text = "Til søknad"
+              className = {_s.ingressButton}
+              disabled = { this.state.disabled }
+            >
+            </Button>
+          </div>
+        </div>
+      )
+    }else{
+      return(
+        <div className={_s.front}>
+          <div className={classnames(_s.card, {[_s.disabled]: this.state.disabled})}>
+              <div className={_s.top}>
+                <img src="../app/src/assets/images/online-icon-white.png"/>
+                <span>{this.state.title}</span>
+              </div>
+              <p>{this.state.short}</p>
+            <Button
+              text = "Til søknad"
+              className = {_s.cardButton}
+              disabled = { this.state.disabled }
+            >
+            </Button>
+          </div>
+        </div>
+      )
+    }
+  }
 }
+
+export default Card;
+//Latest change added ingress
+// TODO:
+// Better way to send the ingress from home to cards?
--- app/src/components/views/Home.jsx
+++ app/src/components/views/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
 import classNames from 'classnames';
-import { Card } from 'components/misc/Card';
+import Card from 'components/misc/Card';
 
 import _s from 'assets/css/Home2.scss';
 
@@ -13,14 +13,17 @@ class Home extends React.Component{
 
   render() {
     let cards = [];
-    for (var i = 0; i < 7; i++) {
+    for (var i = 0; i < 5; i++) {
       cards.push(
         <Card
           title = "Søknadsnavn"
           key = { i }
           disabled = { i % 2 == 0 }
+          short = "Lorem ipsum Dolor sit Amet"
         >
-          <p>Lorem ipsum Dolor sit Amet</p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sit amet ex aliquet, congue dolor ut, maximus lorem. Pellentesque id massa laoreet, hendrerit purus non, posuere dolor. Proin diam velit, posuere et felis et, dapibus laoreet nunc. Sed ultricies laoreet ante ac feugiat. Nam ac laoreet sem. Nulla nec lectus nec lectus cursus ullamcorper. Maecenas erat enim, accumsan sit amet maximus vel, pellentesque sed neque. Maecenas nec dolor feugiat, rutrum risus sit amet, mattis lectus. Vestibulum bibendum ipsum vitae libero efficitur, a sollicitudin metus consectetur. Aliquam aliquam tellus eget purus facilisis hendrerit. Aenean eu ullamcorper nulla. Nunc semper arcu nibh, non tempor augue sagittis eget. Sed eu quam eu odio viverra consequat.
+
+        Morbi eu tortor id orci placerat ultrices ac id ante. Nam iaculis nec odio vel pellentesque. Nunc ullamcorper mi sit amet nisl vulputate pulvinar. Vivamus turpis urna, mattis vitae massa vitae, varius fringilla orci. Aliquam quis orci eu urna euismod consectetur eget cursus nulla. In id eros est. Aenean pulvinar condimentum diam ac bibendum. Aliquam imperdiet feugiat nisi, at elementum mi eleifend et. In diam ligula, accumsan in nisi eu, pharetra finibus sapien. Maecenas lacinia velit sapien, eget tristique ante vulputate a. Donec vitae magna quis quam congue cursus eget vel nibh. Vestibulum dapibus, ex a tristique tincidunt, nisi tortor varius dolor, vehicula mollis dui velit at est. Praesent dictum ipsum a tellus congue, dapibus elementum nisl tristique. Suspendisse pellentesque iaculis lobortis.
         </Card>
       );
     }
