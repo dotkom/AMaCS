c1e9bc42ac2779bafbcc0f8a1f4e1b64a5f26214
oleast <oleast@stud.ntnu.no>
2020-03-31 08:20:45 +0200

    Purge defaultProps in favor of destructuring default values

--- src/components/misc/Selectable.jsx
+++ src/components/misc/Selectable.jsx
@@ -5,19 +5,13 @@ import classNames from 'classnames';
 import _s from 'assets/css/Selectable.module.scss';
 import { API_SETTINGS } from 'common/constants';
 
-const Selectable = ({ committee, onClick, small, selected }) => {
+const Selectable = ({ committee, onClick = null, small = false, selected = false }) => {
   return (
     <div className={classNames(_s.container, { [_s.small]: small, [_s.selected]: selected })} onClick={onClick}>
       <img className={_s.img} src={`${API_SETTINGS.host}${committee.image.md}`} alt={committee.name_long} />
       <h2 className={classNames(_s.name)}>{committee.name_short}</h2>
     </div>
   );
-};
-
-Selectable.defaultProps = {
-  small: false,
-  onClick: null,
-  selected: false,
 };
 
 Selectable.propTypes = {
